7f72c4d8ebb21b4a5580bb94dd4164747e26c0ec
Wayne8983 <nyutukubucha@gmail.com>
2026-03-06 13:33:32 +0300

    Second commit

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h2>Welcome </h2>
-          <p></p>
-          <button>Logout</button>
-        </div>
+    <div className='min-h-screen bg-gray-300 flex flex-col items-center justify-center' >
+      <div className='h-120 w-120 items-center justify-center bg-transparent rounded-2xl shadow-lg ' >
+        <h2 className='text-center font-black font-bold'>Welcome </h2>
+        <button>Logout</button>
       </div>
     </div>
   )
